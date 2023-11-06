caa85682807d1301936025bfc667a80de2edac02
Gabriel Mascarenhas <gabrielmascsousa@gmail.com>
2023-11-06 00:03:47 -0300

    Updated DFA project and image. Implemented the Analex.c

---
+++ Analex.c
@@ -0,0 +1,394 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "Analex.h"
+
+#define LEXEME_SIZE 50
+#define STRING_SIZE 100
+#define NUM_SIZE 20
+
+
+
+TOKEN Analex(FILE *fd) {
+
+    int state;
+
+    char lexeme[LEXEME_SIZE] = "";
+    int sizeL = 0;
+    
+    char digits[NUM_SIZE] = "";
+    int sizeD = 0;
+
+    char string[STRING_SIZE] = "";
+    int sizeS = 0;
+
+    state = 0;
+    t.processed = false;
+    while (true) {
+        char c = fgetc(fd);
+        switch (state) {
+            case 0: if (c == ' ' || c == '\t') state = 0;
+                    else if(c == "\"") {
+                        state = 34;
+                    }
+                    else if(c >= '0' && c <= '9') {
+                        state = 1;
+                        digits[sizeD] = c;
+                        digits[++sizeD] = '\0';
+                    }
+                    else if(c >= 'a' && c<= 'z') {
+                        state = 5;
+                        lexeme[sizeL] = c;
+                        lexeme[++sizeL] = '\0';
+                    }
+                    else if(c == '_') {
+                        state = 6;
+                        lexeme[sizeL] = c;
+                        lexeme[++sizeL] = '\0';
+                    }
+                    else if(c == '|') {
+                        state = 25;
+                    }
+                    else if(c == '&') {
+                        state = 18;
+                    }
+                    else if(c == '>') {
+                        state = 22;
+                    }
+                    else if(c == '=') {
+                        state = 23;
+                    }
+                    else if(c == '<') {
+                        state = 20;
+                    }
+                    else if(c == '!') {
+                        state = 17;
+                    }
+                    else if(c == '/') {
+                        state = 8;
+                    }
+                    else if(c == '*') {
+                        state = 7;
+                        t.cat = SYMBOL;
+                        t.sy_code = MULT;
+                        return t;
+                    }
+                    else if(c == '+') {
+                        state = 9;
+                        t.cat = SYMBOL;
+                        t.sy_code = ADD;
+                        return t;
+                    }
+                    else if(c == '-') {
+                        state = 10;
+                        t.cat = SYMBOL;
+                        t.sy_code = SUBT;
+                        return t;
+                    }
+                    else if(c == '(') {
+                        state = 11;
+                        t.cat = SYMBOL;
+                        t.sy_code = OPEN_PAR;
+                        return t;
+                    }
+                    else if(c == ')') {
+                        state = 12;
+                        t.cat = SYMBOL;
+                        t.sy_code = CLOSE_PAR;
+                        return t;
+                    }
+                    else if(c == '[') {
+                        state = 13;
+                        t.cat = SYMBOL;
+                        t.sy_code = OPEN_BRACK;
+                        return t;
+                    }
+                    else if(c == ']') {
+                        state = 14;
+                        t.cat = SYMBOL;
+                        t.sy_code = CLOSE_BRACK;
+                        return t;
+                    }
+                    else if(c == '{') {
+                        state = 15;
+                        t.cat = SYMBOL;
+                        t.sy_code = OPEN_BRACE;
+                        return t;
+                    }
+                    else if(c == '}') {
+                        state = 16;
+                        t.cat = SYMBOL;
+                        t.sy_code = CLOSE_BRACE;
+                        return t;
+                    }
+                    else if(c == '\'') {
+                        state = 30;
+                    }
+                    else {
+                        printf("Invalid character!");
+                        exit(1);
+                    }
+                    break;
+            case 34: if(isprint(c) != 0) {
+                        state = 35;
+                        string[sizeS] = c;
+                        string[++sizeS] = '\0';
+                    }
+                    else {
+                        printf("Invalid character!");
+                        exit(1);
+                    }
+                    break;
+            case 35: if(isprint(c) != 0 && c != '\n' && c != '\"') {
+                        state = 35;
+                        string[sizeS] = c;
+                        string[++sizeS] = '\0';
+                    }
+                    else if(c == '\n') {
+                        state = 35;
+                    }
+                    else if(c == '\"'){
+                        state = 36;
+                        t.cat = CONST_STR;
+                        strcpy(t.string, string);
+                        return t;
+                    }
+                    else {
+                        printf("Invalid character!");
+                        exit(1);
+                    }
+                    break;
+            case 1: if(c == '.') {
+                        state = 3;
+                        digits[sizeD] = c;
+                        digits[++sizeD] = '\0';
+                    }
+                    else {
+                        state = 38;
+                        ungetc(c, fd);
+                        t.cat = CONST_INT;
+                        t.intVal = atoi(digits);
+                        return t;
+                    }
+                    break;
+            case 3: if(c >= '0' && c <= '9') {
+                        state = 4;
+                        digits[sizeD] = c;
+                        digits[++sizeD] = '\0';
+                    }
+                    else {
+                        printf("Invalid character!");
+                        exit(1);
+                    }
+                    break;
+            case 4: if(c >= '0' && c <= '9') {
+                        state = 4;
+                        digits[sizeD] = c;
+                        digits[++sizeD] = '\0';
+                    }
+                    else {
+                        state = 37;
+                        ungetc(c, fd);
+                        t.cat = CONST_REAL;
+                        t.realVal = atof(digits);
+                        return t;
+                    }
+                    break;
+            case 5: if((c >= '0' && c <= '9') || (c >= 'a' && c<= 'z') || c == '_') {
+                        state = 5;
+                        lexeme[sizeL] = c;
+                        lexeme[++sizeL] = '\0';
+                    }
+                    else {
+                        state = 2;
+                        ungetc(c, fd);
+                        t.cat = ID;
+                        strcpy(t.lexeme, lexeme);
+                        return t;
+                    }
+                    break;
+            case 6: if(c == '_') {
+                        state = 6;
+                        lexeme[sizeL] = c;
+                        lexeme[++sizeL] = '\0';
+                    }
+                    else if(c >= 'a' && c<= 'z') {
+                        state = 5;
+                        lexeme[sizeL] = c;
+                        lexeme[++sizeL] = '\0';
+                    }
+                    else {
+                        printf("Invalid character!");
+                        exit(1);
+                    }
+                    break;
+            case 25: if(c == "|") {
+                        state = 26;
+                        t.cat = SYMBOL;
+                        t.sy_code = OR;
+                        return t;
+                    }
+                    else {
+                        printf("Invalid character!");
+                        exit(1);
+                    }
+                    break;
+            case 18: if(c == "&") {
+                        state = 24;
+                        t.cat = SYMBOL;
+                        t.sy_code = AND;
+                        return t;
+                    }
+                    else {
+                        state = 39;
+                        ungetc(c, fd);
+                        t.cat = SYMBOL;
+                        t.sy_code = ADDR_OF;
+                        return t;
+                    }
+                    break;
+            case 22: if(c == '=') {
+                        state = 43;
+                        t.cat = SYMBOL;
+                        t.sy_code = GREATER_EQ;
+                        return t;
+                    }
+                    else {
+                        state = 40;
+                        ungetc(c, fd);
+                        t.cat = SYMBOL;
+                        t.sy_code = GREATER;
+                        return t;
+                    }
+                    break;
+            case 23: if(c == '=') {
+                        state = 45;
+                        t.cat = SYMBOL;
+                        t.sy_code = EQUAL;
+                        return t;
+                    }
+                    else {
+                        state = 46;
+                        ungetc(c, fd);
+                        t.cat = SYMBOL;
+                        t.sy_code = ASSIGN;
+                        return t;
+                    }
+                    break;
+            case 20: if(c == '=') {
+                        state = 44;
+                        t.cat = SYMBOL;
+                        t.sy_code = LESS_EQ;
+                        return t;
+                    }
+                    else {
+                        state = 42;
+                        ungetc(c, fd);
+                        t.cat = SYMBOL;
+                        t.sy_code = LESS;
+                        return t;
+                    }
+                    break;
+            case 17: if(c == '=') {
+                        state = 21;
+                        t.cat = SYMBOL;
+                        t.sy_code = NOT_EQ;
+                        return t;
+                    }
+                    else {
+                        state = 41;
+                        ungetc(c, fd);
+                        t.cat = SYMBOL;
+                        t.sy_code = NOT;
+                        return t;
+                    }
+                    break;
+            case 8: if(c == '/') {
+                        state = 27;
+                    }
+                    else {
+                        state = 19;
+                        ungetc(c, fd);
+                        t.cat = SYMBOL;
+                        t.sy_code = DIV;
+                        return t;
+                    }
+                    break;
+            case 27: if(c == '\n') {
+                        state = 28;
+                        t.cat = COMMENT;
+                        strcpy(t.string, string);
+                        return t;
+                    }
+                    else {
+                        state = 27;
+                        string[sizeS] = c;
+                        string[++sizeS] = '\0';
+                    }
+                    break;
+            case 30: if(c == '\\') {
+                        state = 31;
+                        string[sizeS] = c;
+                        string[++sizeS] = '\0';
+                    }
+                    else if(isprint(c) != 0 && c != '\'') {
+                        state = 29;
+                        string[sizeS] = c;
+                        string[++sizeS] = '\0';
+                    }
+                    else {
+                        printf("Invalid character on CONST_CHAR!");
+                        exit(1);
+                    }
+                    break;
+            case 29: if(c == '\'') {
+                        state = 47;
+                        t.cat = CONST_CHAR;
+                        strcpy(t.charVal, string);
+                        return t;
+                    }
+                    else {
+                        printf("Invalid character on CONST_CHAR!");
+                        exit(1);
+                    }
+                    break;
+            case 31: if(c == 'n') {
+                        state = 32;
+                        string[sizeS] = c;
+                        string[++sizeS] = '\0';
+                    }
+                    else if(c == '0') {
+                        state = 33;
+                        string[sizeS] = c;
+                        string[++sizeS] = '\0';
+                    }
+                    else {
+                        printf("Invalid character on CONST_CHAR!");
+                        exit(1);
+                    }
+                    break;
+            case 32: if(c == '\'') {
+                        state = 48;
+                        t.cat = CONST_CHAR;
+                        strcpy(t.charVal, string);
+                        return t;
+                    }
+                    else {
+                        printf("Invalid character on CONST_CHAR!");
+                        exit(1);
+                    }
+                    break;
+            case 33: if(c == '\'') {
+                        state = 49;
+                        t.cat = CONST_CHAR;
+                        strcpy(t.charVal, string);
+                        return t;
+                    }
+                    else {
+                        printf("Invalid character on CONST_CHAR!");
+                        exit(1);
+                    }
+                    break;
+        }
+    }
+}
---
+++ Analex.h
@@ -0,0 +1,38 @@
+#ifndef ANALEX
+#define ANALEX
+
+#include <stdio.h>
+#include <stdbool.h>
+
+#define LEXEME_MAX_SIZE 31
+#define STRING_MAX_SIZE 100
+#define COMMENT_MAX_SIZE 300
+
+enum TOKEN_CAT {ID=0, SYMBOL, CONST_INT, CONST_REAL, CONST_CHAR, CONST_STR, COMMENT};
+
+enum SYMBOLS {ASSIGN=0, ADD, SUBT, MULT, DIV, PLUS, MINUS, ADDR_OF,
+                EQUAL, GREATER, LESS, GREATER_EQ, LESS_EQ, NOT_EQ, 
+                AND, OR, NOT, OPEN_PAR, CLOSE_PAR, OPEN_BRACK, 
+                CLOSE_BRACK, OPEN_BRACE, CLOSE_BRACE};
+
+typedef struct{
+    enum TOKEN_CAT cat;
+    bool processed;
+    union {
+        int sy_code; //OPERATOR
+        char lexeme[LEXEME_MAX_SIZE]; //ID
+        int intVal; //CONST_INT
+        float realVal; //CONST_REAL
+        char charVal; //CONST_CHAR
+        char string[STRING_MAX_SIZE]; //CONST_STR
+        char comment[COMMENT_MAX_SIZE]; //COMMENT
+    };
+} TOKEN;
+
+extern TOKEN t;
+extern FILE *fd;
+extern int lineCount;
+
+TOKEN Analex(FILE *);
+
+#endif
